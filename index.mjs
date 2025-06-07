33c4b852f69a9aa3cb6d31945033df9892778ba2
Tilman <github@medallyon.me>
2025-06-07 16:53:23 +0200

    Add logging

--- index.mjs
+++ index.mjs
@@ -9,6 +9,19 @@ global.__dirname = dirname(__filename);
 
 const APPS_PATH = join(__dirname, "apps");
 
+function log(message)
+{
+	if (message !== "")
+		console.log(message);
+
+	const logFilePath = join(__dirname, "app-launcher.log");
+	const timestamp = new Date().toISOString();
+	const logMessage = `${timestamp} - ${message}\n`;
+
+	fs.appendFile(logFilePath, logMessage)
+		.catch(err => console.error("Failed to write to log file:", err));
+}
+
 function startsWithinTheNextSecond(schedule)
 {
 	const cronTime = new CronTime(schedule, "Europe/London");
@@ -19,30 +32,53 @@ function startsWithinTheNextSecond(schedule)
 
 (async function()
 {
-	const appGroups = await fs.readdir(APPS_PATH);
+	log("");
+
+	let dirs = await fs.readdir(APPS_PATH);
+	const appGroups = [];
+
+	// Filter to only include directories
+	for (const dir of dirs)
+	{
+		const groupPath = join(APPS_PATH, dir);
+		if ((await fs.stat(groupPath)).isDirectory()) {
+			appGroups.push(dir);
+		}
+	}
+
 	if (appGroups.length === 0)
-		throw new Error("No apps found");
+	{
+		log("No app groups found.");
+		return;
+	}
+	log(`Found app groups: '${appGroups.join("', '")}'`);
 
 	for (const groupName of appGroups)
 	{
-		const groupPath = join(APPS_PATH, groupName);
-		if (!(await fs.stat(groupPath)).isDirectory())
-			continue;
+		log(`\nProcessing app group: ${groupName}`);
 
+		const groupPath = join(APPS_PATH, groupName);
 		const schedule = (await fs.readFile(join(groupPath, "schedule.txt"), "utf8")).trim();
 
 		if (!startsWithinTheNextSecond(schedule))
+		{
+			log(`Skipping "${groupName}" as the schedule does not match ('${schedule}').`);
 			continue;
+		}
 
 		const appNames = (await fs.readdir(groupPath)).filter(name => name !== "schedule.txt");
 		let failedApps = {};
 
 		for (const appName of appNames)
 		{
+			log(`Launching [${groupName}] ${appName}`);
 			exec(`start "" "${join(groupPath, appName)}"`, e => e && (failedApps[appName] = e));
 		}
 
 		if (Object.keys(failedApps).length > 0)
+		{
+			log(`Failed to launch some apps in group "${groupName}": ${JSON.stringify(failedApps)}`);
 			console.error(`Failed to launch apps from group "${groupName}": ${failedApps}`);
+		}
 	}
 })();
